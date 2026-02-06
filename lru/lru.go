0c1a2e1ca89559bc539b253bc9d6fd12862398d8
Beaten <1578940127@qq.com>
2026-02-06 18:08:25 +0800

    Initial commit

--- lru/lru.go
+++ lru/lru.go
@@ -1,1 +1,76 @@
 package lru
+
+import (
+	"container/list"
+)
+
+type Cache struct {
+	maxBytes  int64 // 缓存允许的最大字节数
+	nbytes    int64 //当前缓存已使用的byte
+	ll        *list.List
+	cache     map[string]*list.Element
+	OnEvicted func(key string, value Value) //可选回调函数
+}
+type entry struct {
+	key   string
+	value Value
+}
+
+// value接口
+type Value interface {
+	Len() int
+}
+
+// 实例化
+func New(maxBytes int64, onEvicted func(string, Value)) *Cache {
+	return &Cache{
+		maxBytes:  maxBytes,
+		ll:        list.New(),
+		cache:     make(map[string]*list.Element),
+		OnEvicted: onEvicted,
+	}
+}
+
+//function:find
+func (c *Cache) Get(key string) (value Value, ok bool) {
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		return kv.value, true
+	}
+	return
+}
+
+// delete
+func (c *Cache) RemoveOldest() {
+	ele := c.ll.Back()
+	if ele != nil {
+		c.ll.Remove(ele)
+		kv := ele.Value.(*entry)
+		delete(c.cache, kv.key)
+		c.nbytes -= int64(len(kv.key)) + int64(kv.value.Len())
+		if c.OnEvicted != nil {
+			c.OnEvicted(kv.key, kv.value)
+		}
+	}
+}
+
+// add or update
+func (c *Cache) Add(key string, value Value) {
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		c.nbytes += int64(value.Len()) - int64(kv.value.Len())
+		kv.value = value
+	} else {
+		ele := c.ll.PushFront(&entry{key, value})
+		c.cache[key] = ele
+		c.nbytes += int64(len(key)) + int64(value.Len())
+	}
+	for c.maxBytes != 0 && c.maxBytes < c.nbytes {
+		c.RemoveOldest()
+	}
+}
+func (c *Cache) Len() int {
+	return c.ll.Len()
+}
